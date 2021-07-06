150859f65940b0ed91da80ab35d0028adba1d86e
AjiBanyu <ajibanyu15@gmail.com>
2021-07-06 11:59:48 +0700

    website portfolio

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+// const menuToggle = document.getElementById('nav-input');
+// const nav = document.getElementById('nav-list');
+
+// menuToggle.addEventListener('click', () => {
+//     nav.classList.toggle('show__menu')
+//     // console.log('slide berhasil');
+// })
